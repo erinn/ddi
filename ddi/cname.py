3f5afd77e95aebe3152e2fbc206dec7dc5d53da6
Erinn Looney-Triggs <erinn.looneytriggs@gmail.com>
2019-02-03 21:31:10 -0500

    Add cname delete.

--- ddi/cname.py
+++ ddi/cname.py
@@ -1,6 +1,5 @@
 from ddi.cli import cli
 from ddi.host import get_host
-from ddi.utilites import query_string_to_dict
 import click
 import jsend
 import json
@@ -27,14 +26,37 @@ def add_cname(cname: str, host_data: dict, session: object, url: str):
     """
     logger.debug('Add cname: %s called on host: %s', cname, host_data['name'])
 
-    aliases = host_data['ip_alias'].split(',')
-
     payload = {'ip_id': host_data['ip_id'], 'ip_name': cname}
     r = session.put(url + 'ip_alias_add', json=payload)
 
     r.raise_for_status()
 
     logger.debug('Add cname result code: %s, JSON: %s', r.status_code, r.json())
+
+    return r.json()
+
+
+def delete_cname(cname: str, host_data: dict, session: object, url: str):
+    """
+    Delete a CNAME from a host.
+
+    :param str cname: The CNAME to add.
+    :param dict host_data: The host data for the host.
+    :param object session: The requests session object.
+    :param str url: The full URL of the DDI server.
+    :return: The response as JSON
+    :rtype: list
+    """
+
+    logger.debug('Delete cname: %s called on host: %s', cname, host_data['name'])
+
+    payload = {'ip_id': host_data['ip_id'], 'ip_name': cname}
+
+    r = session.delete(url + 'ip_alias_delete', json=payload)
+
+    r.raise_for_status()
+
+    logger.debug('Delete cname result code: %s, JSON: %s', r.status_code, r.json())
 
     return r.json()
 
@@ -86,12 +108,28 @@ def add(ctx, host, cname):
 
 
 @cname.command()
+@click.argument('cname', envvar='DDI_CNAME_DELETE_CNAME', nargs=1)
+@click.pass_context()
+def delete(ctx, cname):
+    """Delete a single CNAME entry for a host"""
+    host_data = get_cname_info(cname, ctx.obj['session'], ctx.obj['url'])[0]
+    r = delete_cname(cname, host_data, ctx.obj['session'], ctx.obj['url'])
+
+    if ctx.obj['json']:
+        data = jsend.success(r)
+        click.echo(json.dumps(data, indent=2, sort_keys=True))
+    else:
+        click.echo(f"CNAME: {cname} deleted from host: {host_data['name']}.")
+
+
+@cname.command()
 @click.argument('cname', envvar='DDI_CNAME_INFO_CNAME', nargs=1)
 @click.pass_context
 def info(ctx, cname):
     """Retrieve the host info associated with a cname"""
 
     host_data = get_cname_info(cname, ctx.obj['session'], ctx.obj['url'])[0]
+
     if ctx.obj['json']:
         data = jsend.success(host_data)
         click.echo(json.dumps(data, indent=2, sort_keys=True))
